822a09ef98f66facf0be000d4e960f25f970ffe2
scotwith1tee <96700496+scotwith1tee@users.noreply.github.com>
2022-12-08 06:39:42 -0600

    Part way through Part 1

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -4,16 +4,37 @@ def MoveCrates(currMove):
     """Move the crates according to the move command """
     # Loop through each character and find the occurrences in the string
     # If there's more than 1, it's not unique
-    for i in range(len(testStr)):
-        letter = testStr[i]
-        # We will always find one
-        index = testStr.find(letter)
-        if testStr.find(letter,index+1) > 0:
-            # We found a duplicate
-            return False
+    pass
+
+def FindNumbersInString(string):    
+    """Move the crates according to the move command """
+    res = [int(i) for i in string.split() if i.isdigit()]
+    return res
+
+def PopulateStartingPileArrays(pileStr,numPiles):
+    startIndex = 1
+    stepIndex = 4
+    rowIndex = 0
+    piles = []
+
+    #Create a 2d array where [pile][stack]
+    # the stack list is built from the bottom to the top
+    for pile in range(0,numPiles):
+        stackList = []
+        for height in range(len(pileStr)-1,-1,-1):
+            #print(height)
+            #print(pileStr[height])
+            rowStr = pileStr[height]
+            char = rowStr[startIndex + (stepIndex*pile)]
+            if char.isalpha():
+                # it's a character, put it on the 
+                #print(char)
+                #print('pile=',pile,'row=', rowIndex)
+                stackList.append(char)
+                 #Put the crates in the rows and piles
     
-    # No duplicates, it's unique
-    return True
+        piles.append(stackList)
+    print(piles)    
 
 print("The date and time is:",datetime.datetime.now())
 
@@ -23,58 +44,23 @@ with open('testinput.txt','r') as f:
 f.close()
 
 # find crate configuration
-# find move list
+for i in range(len(array)):
+    if(array[i].find(' 1 ',0,4))>=0 :
+        print(array[i])
+        numPiles = max(FindNumbersInString(array[i]));
+        print('Num Piles:',numPiles)
+        pileConfigStr = array[0:i]
+        # find move list
+        moveList = array[i+1:]
+        print(pileConfigStr)
+        print('')
+        print('')
+        print('')
+        print(moveList)
+        break
+
 # create a 2d list array for crate configuration
+PopulateStartingPileArrays(pileConfigStr,numPiles)
 # top crate is the last item on the crate list
 # Send the moves 1 at a time to modify the crate arrays
 # test find all numbers in a string
-test_string = ' 1   2   3   4   5   6   7   8   9'
-res = [int(i) for i in test_string.split() if i.isdigit()]
-print(res) 
-test_string = 'move 14 from 1 to 2'
-res = [int(i) for i in test_string.split() if i.isdigit()]
-print(res) 
-print(type(res))
-print(len(res))
-print(res[0])
-print(type(res[0]))
-print(res[1])
-print(res[2])
-
-print(array)
-print(len(array))
-print(len(array[0]))
-print(type(array[0]))
-#Part 1
-# Find the first 4 non identical letters
-sum = 0;
-myStr = array[0]
-print('Input Length:',len(myStr))
-#myStr = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
-for index in range(len(myStr)):
-    # the index is the starting character
-    # load four characters, starting with the index
-    tmpStr = myStr[index:index+4]
-
-    if IsStrUniqueChars(tmpStr):
-        print('Part 1:Start of Packet:',index+4)
-        break
-
-
-#Part 2 - Find the Start of Message marker. Fourteen non identical letters
-#myStr = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'
-for index in range(len(myStr)):
-    # the index is the starting character
-    # load four characters, starting with the index
-    tmpStr = myStr[index:index+14]
-
-    if IsStrUniqueChars(tmpStr):
-        print('Part 2:Start of message:',index+14)
-        break
-
-#    print(tmpStr)
-    # Determine points for that round
-#    print(myStr[index])
-
-    # See if we are at the end of an elfs list. add another list item
-
