ca713432da5816ca3d2acfc94e6301a04e3df82b
scotwith1tee <96700496+scotwith1tee@users.noreply.github.com>
2022-12-17 10:05:00 -0600

    Save before I screw everything up

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -17,6 +17,21 @@ def CalcWorryLevel(num, oper, operand):
     result = result // 3
     return int(result)
 
+def CalcWorryLevelNoDiv(num, oper, operand):    
+    """Perform the opeation on the number then div 3 and round"""
+    if oper == '+':
+        if operand == 'old':
+            result = 2 * num
+        else:
+            result = num + int(operand)
+    elif oper == '*':
+        if operand == 'old':
+            result = num * num
+        else:
+            result = num * int(operand)
+    # get the floor of a divide by 3
+    #result = result // 3
+    return int(result)
 
 def FindNumbersInString(string):    
     """Find all positive AND negative integers in a string """
@@ -38,27 +53,7 @@ with open('testinput.txt','r') as f:
     array = f.readlines()
 f.close()
 
-#Make the first dictionary item
-# dictTemplate = dict(ID = 0, items = [9999], operation = "*", operand = "0", test = 51001, true = 100, false = 100)
-# dictTemplate1 = dict(ID = 1, items = [9999], operation = "*", operand = "0", test = 51001, true = 100, false = 100)
-# monkeys = [dictTemplate,dictTemplate1]
-# print("monkeys type: ", type(monkeys))
-# print(monkeys)
-# print("<<<<<<<")
-# monkeys.append(dictTemplate)
-# print(monkeys)
-# print("<<<<<<<")
-# monkeys[0]['ID'] = 2
-# monkeys +=dictTemplate
-# print(monkeys)
-# print("monkeys type: ", type(monkeys))
-# print("<<<<<<<")
-# monkeys.append(dictTemplate)
-# print(monkeys)
-# print("monkeys type: ", type(monkeys))
-# print("<<<<<<<")
-# print("<<<<<<<")
-# print("<<<<<<<")
+
 monkeys =[]
 # loop through the input to create the initial monkey dictionary array
 for i in range(len(array)):
@@ -67,11 +62,8 @@ for i in range(len(array)):
     if array[i].find('Monkey')>-1:
         # Grab the id, even though we will access it by the dictionary list number
         monkeys.append({ })
-        print(array[i])
         MonkeyId = FindNumbersInString(array[i])[0]
-        print('Monkey Id: ', MonkeyId)
         monkeys[MonkeyId]["ID"] = MonkeyId
-        print('Monkey Id from Dict:',monkeys[MonkeyId]["ID"])
         # Load items from next line
         monkeys[MonkeyId]["items"] = FindNumbersInString(array[i+1])
         # Load operation and operand
@@ -86,11 +78,64 @@ for i in range(len(array)):
         # Load true and false monkey recipient
         monkeys[MonkeyId]["true"] =  FindNumbersInString(array[i+4])[0]
         monkeys[MonkeyId]["false"] = FindNumbersInString(array[i+5])[0]
-        # print('Monkey Length',len(monkeys))
-        # print(monkeys)
-        # if len(monkeys) == 2:
-        #     break
+monkeyOrig = monkeys # Save for later
+print(*monkeys, sep='\n')
+print(" ")
+print(" Start throwning items")
+total = [0] * (len(monkeys)+1)
+for rnd in range(20):
+    for m in range(len(monkeys)):
+        for items in range(len(monkeys[m]['items'])):
+            total[m] += 1
+            currItem = monkeys[m]['items'][items]
+            wLevel = CalcWorryLevel(currItem, monkeys[m]['operation'], monkeys[m]['operand'])
+            if (wLevel % int(monkeys[m]['test'])) == 0:
+                target = int(monkeys[m]['true'])
+            else:
+                target = int(monkeys[m]['false'])
+            # Move the item to the target monkey
+            monkeys[target]['items'].append(wLevel)
 
-print(monkeys)
+        #empty the list at the end because they've thrown all their items
+        monkeys[m]['items'].clear()
+        #print(*monkeys, sep='\n')
+        #print(monkeys)
+print(total)
+
+total.sort()
+print(total[-1]*total[-2])
+print(total)
+print(*monkeys, sep='\n')
+
+print(" Part 2: start with orig, no divide by three")
+monkeys = monkeyOrig
+total = [0] * (len(monkeys)+1)
+for rnd in range(20):
+    for m in range(len(monkeys)):
+        for items in range(len(monkeys[m]['items'])):
+            total[m] += 1
+            currItem = monkeys[m]['items'][items]
+            wLevel = CalcWorryLevel(currItem, monkeys[m]['operation'], monkeys[m]['operand'])
+            if (wLevel % int(monkeys[m]['test'])) == 0:
+                target = int(monkeys[m]['true'])
+            else:
+                target = int(monkeys[m]['false'])
+            # Move the item to the target monkey
+            monkeys[target]['items'].append(wLevel)
+
+        #empty the list at the end because they've thrown all their items
+        monkeys[m]['items'].clear()
+        #print(*monkeys, sep='\n')
+        #print(monkeys)
+    if(rnd % 1000)==0:
+        print(rnd)
+        print(total)
+
+print(total)
+
+total.sort()
+print(total[-1]*total[-2])
+print(total)
+print(*monkeys, sep='\n')
 
 
